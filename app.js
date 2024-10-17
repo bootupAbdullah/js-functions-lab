74805ffb4ab02c094894ac043d7ae027958d071a
Abdulla Durrani <Abdullahkd@gmail.com>
2024-10-17 16:16:57 -0400

    completed excercise 4: generateEmail()

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ const maxOfTwoNumbers = (x, y) => {
   console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
 
 
-//Exercise 2: isAdult()
-
 /*
 Exercise 2: isAdult()
 
@@ -42,6 +40,45 @@ function isAdult(number){
 console.log('Exercise 2 Result:', isAdult(21));
 
 
+/*
+Exercise 3: isCharAVowel()
 
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+function isCharAVowel(letter) {
+    if (letter === ("a", "e", "i", "o", "u")) {
+        return true
+    }
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+
+
+function generateEmail(name, domain) {
+    return email = `${name}@${domain}`
+}
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
 
 
