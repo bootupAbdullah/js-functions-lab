93d3fb28d6e45048527116da4efdc7713aeae5ff
Abdulla Durrani <Abdullahkd@gmail.com>
2024-10-17 20:42:11 -0400

    completed excercise 5: greetUser()'
    q

--- app.js
+++ app.js
@@ -60,8 +60,7 @@ function isCharAVowel(letter) {
         return false
     }
 }
-
-console.log('Exercise 3 Result:', isCharAVowel("b"));
+console.log('Exercise 3 Result:', isCharAVowel("a"));
 
 /*
 Exercise 4: generateEmail()
@@ -83,3 +82,22 @@ function generateEmail(name, domain) {
 console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
 
 
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+
+
+const greetUser = (name, timeOfDay) => {
+    return `Good ${timeOfDay} ${name}!`
+}
+
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
